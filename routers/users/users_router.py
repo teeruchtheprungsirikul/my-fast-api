c7a1b3a063454946a422665effbc303bf009f34d
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2023-12-05 04:53:53 +0700

    complete user router

--- routers/users/users_router.py
+++ routers/users/users_router.py
@@ -12,10 +12,26 @@ from routers.users import user_controller
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/",response_model=List[UserDisplayBase])
+@router.get("/", response_model=List[UserDisplayBase])
 def get_all_user(db: Session = Depends(get_db)):
     return user_controller.read_users(db)
 
+
+@router.get("/{id}")
+def user_by_id(id: int, db: Session = Depends(get_db)):
+    return user_controller.read_user_by_id(db, id)
+
+
 @router.post("/")
-def register_user(request: UserBase, db: Session= Depends(get_db)):
+def register_user(request: UserBase, db: Session = Depends(get_db)):
     return user_controller.create(db, request)
+
+
+@router.put("/{id}")
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return user_controller.update(db, id, request)
+
+
+@router.delete("/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return user_controller.delete(db, id)
